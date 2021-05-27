4a9bf7525d9a7667178e084c0211ff35e990c104
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-27 13:09:02 -0700

    got my entries nav to work

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 /* global data */
 /* exported data */
 
+// I want the entries tab toggle the entries page
+// If there are entries i want it to show them
+// if there are no entries i want the screen to show no entries recorded
+// I need to make the div class with entry tab active
+// that will trigger the new events page to go hidden
+// and also the no entries recorded page to go hidden
+// I need to query the dom for the tab class and view classes
+// set up the addEventListener for the click event with the .tab query
+// i need to define a function that will loop through each tab class and if it
+// is equal to the event.target trigger the entries to go from hidden to view
+// unless there is no entries then the entries page will stay hidden
+// and the no entries recorded page will stay in view.
+
 var $photoUrl = document.querySelector('input.photo-url');
 var $newImg = document.querySelector('.img');
-$photoUrl.addEventListener('input', getPhotoUrl);
 var $form = document.querySelector('.form');
+
+$photoUrl.addEventListener('input', getPhotoUrl);
 $form.addEventListener('submit', getSubmit);
+
 function getPhotoUrl(event) {
   $newImg.setAttribute('src', event.target.value);
 }
@@ -57,10 +72,28 @@ var $Ul = document.querySelector('.ul');
 document.addEventListener('DOMContentLoaded', entryContentLoaded);
 
 function entryContentLoaded(event) {
-  // if (entryContentLoaded !== null) {
   for (var i = 0; i < data.entries.length; i++) {
     var entryObjects = journalEntry(data.entries[i]);
     $Ul.append(entryObjects);
   }
 }
-// }
+var $entry = document.querySelector('.entry');
+var $viewAll = document.querySelectorAll('.view');
+var $noEntries = document.querySelector('.none');
+$entry.addEventListener('click', viewSwitch);
+
+function viewSwitch(event) {
+  var matchingDataView = event.target.getAttribute('data-view');
+  for (var i = 0; i < $viewAll.length; i++) {
+    if ($viewAll[i].getAttribute('data-view') === matchingDataView) {
+      $viewAll[i].className = 'view';
+    } else {
+      $viewAll[i].className = 'view hidden';
+    }
+  }
+  if (data.entries.length === 0) {
+    $noEntries.className = 'none';
+  } else {
+    $noEntries.className = 'none hidden';
+  }
+}
