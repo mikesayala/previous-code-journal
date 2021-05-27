66f399ac2e78ee172f993f27aaa25f3212f6b0c7
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-26 19:57:31 -0700

    use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the DOMContentLoaded event is fired and all the previous tasks that i did not commit before it.

--- js/main.js
+++ js/main.js
@@ -26,3 +26,41 @@ function getSubmit(event) {
     $newImg.setAttribute('src', 'images/placeholder-image-square.jpg');
   }
 }
+
+function journalEntry(entry) {
+  var entryLi = document.createElement('li');
+  entryLi.className = 'column list-style-type';
+
+  var entryImg = document.createElement('img');
+  entryImg.setAttribute('src', entry.photoUrl);
+  entryImg.className = 'column column-full img';
+  entryLi.appendChild(entryImg);
+
+  var entryDivTextContent = document.createElement('div');
+  entryDivTextContent.className = 'text-content';
+  entryLi.appendChild(entryDivTextContent);
+
+  var entryH3 = document.createElement('h3');
+  entryH3.className = 'h3';
+  entryH3.textContent = entry.title;
+  entryDivTextContent.appendChild(entryH3);
+
+  var entryP = document.createElement('p');
+  entryP.textContent = entry.notes;
+  entryP.className = 'text-content';
+  entryDivTextContent.appendChild(entryP);
+
+  return entryLi;
+}
+
+var $Ul = document.querySelector('.ul');
+document.addEventListener('DOMContentLoaded', entryContentLoaded);
+
+function entryContentLoaded(event) {
+  // if (entryContentLoaded !== null) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var entryObjects = journalEntry(data.entries[i]);
+    $Ul.append(entryObjects);
+  }
+}
+// }
