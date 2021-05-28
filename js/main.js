381336ee5d7c172b2fbd6248c776bfa8e867e232
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-28 09:54:04 -0700

    still in progress but i need to fix the container for no recorded entries

--- js/main.js
+++ js/main.js
@@ -13,13 +13,36 @@
 // is equal to the event.target trigger the entries to go from hidden to view
 // unless there is no entries then the entries page will stay hidden
 // and the no entries recorded page will stay in view.
-
+// take view variable compare to div elements with attribute data-view = 'entries' and set them to view and if not entries set to hidden
 var $photoUrl = document.querySelector('input.photo-url');
 var $newImg = document.querySelector('.img');
 var $form = document.querySelector('.form');
-
+var $Ul = document.querySelector('.ul');
+var $noEntry = document.querySelector('.no-entry');
+var $entry = document.querySelector('.entry');
+var $viewAll = document.querySelectorAll('.view');
+var $new = document.querySelector('.new');
+$new.addEventListener('click', newEntry);
+document.addEventListener('DOMContentLoaded', entryContentLoaded);
 $photoUrl.addEventListener('input', getPhotoUrl);
 $form.addEventListener('submit', getSubmit);
+
+window.addEventListener('load', onLoad);
+function onLoad(event) {
+  var view = data.view;
+  for (var i = 0; i < $viewAll.length; i++) {
+    if (view === $viewAll[i].getAttribute('data-view')) {
+      $viewAll[i].className = 'view';
+    } else {
+      $viewAll[i].className = 'view hidden';
+    }
+    if (data.entries.length === 0) {
+      $noEntry.className = 'view';
+    } else {
+      $noEntry.className = 'view hidden';
+    }
+  }
+}
 
 function getPhotoUrl(event) {
   $newImg.setAttribute('src', event.target.value);
@@ -40,6 +63,9 @@ function getSubmit(event) {
   if (getSubmit) {
     $newImg.setAttribute('src', 'images/placeholder-image-square.jpg');
   }
+
+  $Ul.prepend(journalEntry(data.entries[0]));
+  viewSwitch('entries');
 }
 
 function journalEntry(entry) {
@@ -68,42 +94,39 @@ function journalEntry(entry) {
   return entryLi;
 }
 
-var $Ul = document.querySelector('.ul');
-document.addEventListener('DOMContentLoaded', entryContentLoaded);
-
 function entryContentLoaded(event) {
   for (var i = 0; i < data.entries.length; i++) {
     var entryObjects = journalEntry(data.entries[i]);
     $Ul.append(entryObjects);
   }
 }
-var $noEntry = document.querySelector('.no-entry');
-var $entry = document.querySelector('.entry');
-var $viewAll = document.querySelectorAll('.view');
-// var $noEntries = document.querySelector('.none');
-$entry.addEventListener('click', viewSwitch);
-function viewSwitch(event) {
-  var matchingDataView = event.target.getAttribute('data-view');
+
+$entry.addEventListener('click', function (event) {
+  var targetView = event.target.getAttribute('data-view');
+  viewSwitch(targetView);
+});
+
+function viewSwitch(viewName) {
+  data.view = 'entries';
   for (var i = 0; i < $viewAll.length; i++) {
-    if ($viewAll[i].getAttribute('data-view') === matchingDataView) {
+    if ($viewAll[i].getAttribute('data-view') === viewName) {
       $viewAll[i].className = 'view';
     } else {
       $viewAll[i].className = 'view hidden';
     }
   }
-  if (data.entries.length === 0) {
-    $noEntry.className = 'view';
-  } else {
+  if (data.entries.length !== 0) {
     $noEntry.className = 'view hidden';
+  } else {
+    $noEntry.className = 'view';
   }
 }
 // when i click on the new button
 // i need entries to be hidden
 // and the form-container visible
-var $new = document.querySelector('.new');
-$new.addEventListener('click', newEntry);
 
 function newEntry(event) {
+  data.view = 'entry-form';
   var newMatchView = event.target.getAttribute('data-view');
   for (var i = 0; i < $viewAll.length; i++) {
     if ($viewAll[i].getAttribute('data-view') === newMatchView) {
